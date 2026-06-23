3a35ca7289e04b11c5943f7db19d2a764a557d9c
Muhammad Shameer <mohammedshahmir48@gmail.com>
2026-06-24 02:28:40 +0500

    24-june-2026 2:30AM PST CHANGES / FIXES

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -109,7 +109,7 @@ const HeroSection = () => {
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start mb-10">
             <a
-              href="/shop"
+              href="/states"
               ref={(el) => {
                 if (el) staggerRefs.current[0] = el;
               }}
